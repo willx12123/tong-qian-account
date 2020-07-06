a59ef89d07d09954c278bff6ed91667bf3e5d6b1
AerospaceXu <aerospacexu@gmail.com>
2020-07-06 22:01:51 +0800

    更改路径

---
+++ dist/precache-manifest.f95a9f079ab7b81afd92f29ea39335db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a048c3a97724052d35f",
+    "url": "css/app.1d363d15.css"
+  },
+  {
+    "revision": "1d3f5037b1495bdbf4ad",
+    "url": "css/chunk-79015136.02e5e116.css"
+  },
+  {
+    "revision": "88baf51ec40da1df8b1af582c717bf46",
+    "url": "index.html"
+  },
+  {
+    "revision": "5a048c3a97724052d35f",
+    "url": "js/app.16010efe.js"
+  },
+  {
+    "revision": "1d3f5037b1495bdbf4ad",
+    "url": "js/chunk-79015136.3b5b5648.js"
+  },
+  {
+    "revision": "3ea79dce30e243a15dcf",
+    "url": "js/chunk-vendors.a95e7447.js"
+  },
+  {
+    "revision": "7604696ea439528ed146087ec761fd3b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
